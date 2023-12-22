6cbbdf947e5e0acb39a6280248d8c97e03dc0b90
Nguyen Huu Nhan <88808459+nguyenhuunhan1903@users.noreply.github.com>
2023-12-22 16:28:32 +0700

    first commit

---
+++ components/index.tsx
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetail from "./CarDetail";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    CustomButton,
+    Footer,
+    NavBar,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetail,
+    ShowMore
+}
