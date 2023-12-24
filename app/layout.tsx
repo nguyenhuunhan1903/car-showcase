107f48ad31c7b7b2dad99721ed9aa4886fbf4edf
Nguyen Huu Nhan <88808459+nguyenhuunhan1903@users.noreply.github.com>
2023-12-24 09:40:28 +0700

    fix bug key's list

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import { Footer, NavBar } from '@/components'
 export const metadata: Metadata = {
   title: 'Car Hub',
   description: 'Discover the best cars in the world.',
+  icons:{
+    icon: '/model-icon.png',
+  }
 }
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -39,8 +39,8 @@ export default async function Home({searchParams}:{searchParams: FilterProps}) {
         { !isDataEmpty ? (
           <section>
             <div className='home__cars-wrapper'>
-              {allCars?.map((car) => (
-                <CarCard car={car} />
+              {allCars?.map((car,index) => (
+                <CarCard key={index} car={car} />
               ))}
             </div>
             <ShowMore
